4e76b554e8e8803cf0c2fbfe037a5cd96ebfa461
Nikita <bednyakov99@gmail.com>
2021-01-17 10:51:04 +0300

    initial commit

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    const float thrustMoveForwardSpeed = 4;
+    const float thrustRotateSpeed = 3;
+    Rigidbody2D rigidSpaceShip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rigidSpaceShip = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // physically thrust rigidSpaceShip along the direction of looking
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            rigidSpaceShip.AddForce(transform.up * thrustMoveForwardSpeed , ForceMode2D.Force); // transform.up - direction facing
+        }
+
+        // physically rotate rigidSpaceShip around z coordinate
+        float inputRotate = Input.GetAxis("Rotate"); 
+        if (inputRotate != 0)
+        {
+            //rigidSpaceShip.AddTorque(-inputRotate * thrustRotateSpeed, ForceMode2D.Force); // -inputRotate because .AddTorque rotates obj in clockwise direction
+            
+            // rotation without physics
+            transform.Rotate(new Vector3(0, 0, -inputRotate * thrustRotateSpeed));
+        }
+    }
+}
---
+++ Assets/Scripts/ScreenWraper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWraper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnBecameInvisible()
+    {
+        Vector3 position = transform.position;
+        if (position.y > ScreenUtils.ScreenTop || position.y < ScreenUtils.ScreenBottom)
+        {
+            position.y = -position.y;
+        }
+        if (position.x < ScreenUtils.ScreenLeft || position.x > ScreenUtils.ScreenRight)
+        {
+            position.x = -position.x;
+        }
+        transform.position = position;
+    }
+}
